32424613ea90f5f6155a8e4338b6879658a55c02
Sergey Kudryavtsev <kusa@bork.ru>
2022-07-03 19:23:22 +0300

    basic project customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ChakraProvider, Container, Text} from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ChakraProvider>
+      <header className='app-header'>
+        <Container maxW="unset">
+          <Text fontSize="20px" color="white">
+            Effect learn
+          </Text>
+        </Container>
       </header>
-    </div>
+    </ChakraProvider>
   );
 }
 
