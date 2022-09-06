9e589c0b781461fa7cb1e4bdd4fe58f742da15ce
Sergey Kudryavtsev <KudryavtsevSr@yandex.ru>
2022-09-06 22:46:43 +0300

    add edit button

--- src/components/TermsItem/component.tsx
+++ src/components/TermsItem/component.tsx
@@ -1,5 +1,5 @@
 import {Grid, Box, IconButton} from '@chakra-ui/react';
-import {DeleteIcon} from '@chakra-ui/icons';
+import {DeleteIcon, EditIcon} from '@chakra-ui/icons';
 import React, {useState, memo} from 'react';
 import {Form} from '../Form';
 
@@ -29,21 +29,25 @@ export default memo(function Component({term, definition, id, removeTerm}: Prope
 
   return (
     <Box pos="relative">
-      <IconButton pos="absolute" opacity="0.1" top="0" right="0" aria-label="Remove" icon={<DeleteIcon/>}
-                  onClick={removeCurrentTerm}/>
       {
         isFormVisible
           ? (
             <Form initialTerm={term} initialDefinition={definition} id={id} onEdited={hideForm}/>
           )
           : (
-            <Grid templateColumns="repeat(2, 1fr)" gap={6} p={6} bg="gray.100" borderRadius="md"
-                  onDoubleClick={showForm} title="Double click for edit">
-              <div>
-                {term}
-              </div>
-              <div dangerouslySetInnerHTML={{__html: definition}}/>
-            </Grid>
+            <>
+              <IconButton pos="absolute" opacity="0.1" top="40px" right="0" aria-label="Edit" icon={<EditIcon/>}
+                          onClick={showForm}/>
+              <IconButton pos="absolute" opacity="0.1" top="0" right="0" aria-label="Remove" icon={<DeleteIcon/>}
+                          onClick={removeCurrentTerm}/>
+              <Grid templateColumns="repeat(2, 1fr)" gap={6} p={6} bg="gray.100" borderRadius="md"
+                    onDoubleClick={showForm} title="Double click for edit">
+                <div>
+                  {term}
+                </div>
+                <div dangerouslySetInnerHTML={{__html: definition}}/>
+              </Grid>
+            </>
           )
       }
     </Box>
